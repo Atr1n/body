5381240810356d736fede6783725d6fc7f6917d0
maxim <xn_atr1n@mail.ru>
2022-01-12 17:57:53 +0600

    se2

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,18 +1,29 @@
 import Swiper from 'swiper/swiper-bundle';
 import 'swiper/swiper-bundle.css';
 
+const authSwiper = new Swiper(".authSwiper", {
+    slidesPerView: 3,
+    slideToClickedSlide: true,
+    watchOverflow: true,
+    loop: true,
+    centeredSlides: true,
+    
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
 const comSwiper = new Swiper(".comSwiper", {
     slidesPerView: 1,
     slideToClickedSlide: true,
     watchOverflow: true,
 
+    autoplay: {
+        delay: 3000,
+    },
+    
     pagination: {
         el: ".swiper-pagination",
     },
-});
-
-const authSwiper = new Swiper(".authSwiper", {
-    slidesPerView: 1,
-    slideToClickedSlide: true,
-    watchOverflow: true,
 });
